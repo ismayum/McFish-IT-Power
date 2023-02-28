0da29f973a7d937a438b60bb4493dad1a99f4f49
Ismail Ismail <ismail.i7@gso.schule.koeln>
2023-02-28 13:24:33 +0100

    Layout-Datei zum clonen

---
+++ GameLauncher2/GameLauncher/GUI/Pages/PageGames.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameLauncher.GUI.Pages
+{
+    public partial class PageGames : UserControl
+    {
+        public PageGames()
+        {
+            if (Program.IsInDesignMode()) return;
+            InitializeComponent();
+        }
+    }
+}
